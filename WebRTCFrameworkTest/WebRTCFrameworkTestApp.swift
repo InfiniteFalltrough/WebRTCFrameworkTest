e2e46bf016c596c5b2d07be8aa6ee173c050807e
Viktor Golubenkov <vityandra@Viktors-MacBook-Pro.local>
2023-08-22 19:58:51 +0700

    initial

---
+++ WebRTCFrameworkTest/WebRTCFrameworkTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebRTCFrameworkTestApp.swift
+//  WebRTCFrameworkTest
+//
+//  Created by Viktor Golubenkov on 8/22/23.
+//
+
+import SwiftUI
+
+@main
+struct WebRTCFrameworkTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
